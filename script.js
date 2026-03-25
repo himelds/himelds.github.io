fa7c936f8e5b0265737fca49712d559985e6a708
Himel Das <151542219+himelds@users.noreply.github.com>
2026-03-25 22:04:34 +0600

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.addEventListener('load', () => {
 /*==================== TYPING ANIMATION ====================*/
 const typingTextElement = document.querySelector('.typing-text');
 if (typingTextElement !== null) {
-    const titles = ["Aspiring Data Scientist", "AI Research Enthusiast"];
+    const titles = ["a Data Scientist", "an AI Research Enthusiastic"];
     let titleIndex = 0;
     let charIndex = 0;
     let isDeleting = false;
